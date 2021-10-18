113d4ae562a7296df72c97ac56f4b6dd2fdb1458
Alex-tq <alextq3036@gmail.com>
2021-10-18 09:56:09 -0400

    Testing deployment with react-router

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <nav>
-        <Link to="/">Home</Link>
+        <Link exact to="/">Home</Link>
         <Link to="/about">About</Link>
         <Link to="/contact">Contact</Link>
       </nav>
